3ce575ad7bca74f620c619f04268588dfc8dd8a1
Patri <uo282210@uniovi.es>
2023-04-27 00:42:15 +0200

    SDI-2223-304-SW-11.1- Servicio SOAP Cliente.

---
+++ sdi-2223-304-soap-sw-client/src/main/java/com/uniovi/sdi/Sdi2223304SoapSwClientApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223304SoapSwClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223304SoapSwClientApplication.class, args);
+    }
+
+}
